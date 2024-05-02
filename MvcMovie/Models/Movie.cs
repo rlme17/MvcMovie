8e5cd4f710051ede881f247a00624ace3d4d1a06
espinor2 <espinor2@LNGMNLL-CNCLVP2>
2024-05-02 22:35:33 +0800

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models;
+
+[Table("TestMovieRichard")]
+public class Movie
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "Please Input Title")]
+    public string Title { get; set; }
+
+    [DataType(DataType.Date)]
+    [Column("Release_Date")]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    public decimal Price { get; set; }
+}
